2b7a528022b2357592b6ec2e14c7bde23956d298
Alexander Glushchenko <alexgl007@mail.ru>
2024-02-07 13:37:15 +1000

    Init commit

---
+++ PlatformService/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemory"));
+builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+PrepDb.PrepPopulation(app);
+
+app.Run();
